80aa197792b7f89368144953f08df31ac0f2708f
Stackbit Code Editor <projects@stackbit.com>
2022-03-22 06:38:00 +0000

    link.js: changed

--- src/utils/link.js
+++ src/utils/link.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NextLink from 'next/link';
 
 export default function Link({ children, href, ...other }) {
-    // Pass Any internal link to Next.js Link, for anything else, use <a> tag
+    // Передайте любую внутреннюю ссылку в Next.js Link, для всего остального используйте тег <a>
     const internal = /^\/(?!\/)/.test(href);
     if (internal) {
         return (
