989731a86ba11e12317dff310d50c0bc33388d52
Stackbit Code Editor <projects@stackbit.com>
2022-03-22 06:31:20 +0000

    getPages.js: changed

--- src/utils/getPages.js
+++ src/utils/getPages.js
@@ -3,8 +3,7 @@ import _ from 'lodash';
 /**
  * Get all the pages located under the provided `urlPath`, 
  * not including the index page. 
- * I.e.: All pages having their URLs start with `urlPath` excluding
- * the page having its URL equal to `urlPath`.
+ * I.e.: All pages having their URLs start with `urlPath` excludingthe page having its URL equal to `urlPath`.
  *
  * @example
  * pages => [
