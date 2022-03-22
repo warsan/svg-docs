cbb2553d30de815326f8ca8ee4e8b28a49d9d0b7
Stackbit Code Editor <projects@stackbit.com>
2022-03-22 06:51:20 +0000

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ module.exports = {
         // В противном случае webpack перекомпилирует приложение и обновит всю страницу.
         // Вместо этого в src/pages/[...slug].js используется функция "withRemoteDataUpdates".
         // функция для обновления содержимого страницы без обновления всей страницы
+        
         config.plugins.push(new webpack.WatchIgnorePlugin([[/\/content\//]]));
         return config;
     }
